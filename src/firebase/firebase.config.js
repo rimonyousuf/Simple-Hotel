ff53bfe8f245902450f450590a5954df1d029820
Rimon <rimonyousuf00@gmail.com>
2024-01-20 15:15:05 +0600

    Hide firebase config key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBUlhOQVuHQjOKjxCoktLUj4qgoIKn5Tk",
-  authDomain: "burj-al-arab-b3e06.firebaseapp.com",
-  projectId: "burj-al-arab-b3e06",
-  storageBucket: "burj-al-arab-b3e06.appspot.com",
-  messagingSenderId: "1039019795400",
-  appId: "1:1039019795400:web:98ce3f3fec631f1d0fd6fd"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
